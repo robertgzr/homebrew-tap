ca998d455f26d613c4642f1c72543d72b7b467b3
Robert G <robert.guenzler@student.htw-berlin.de>
2016-09-16 12:25:45 +0200

    update Iosevka casks to v1.9.3

--- casks/font-iosevka-slab.rb
+++ casks/font-iosevka-slab.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-slab" do
-  version "1.9.1"
-  sha256 "dd8950614acf01b829db5243d1cde5169d4e153b3aece3b13b470632287ca3bd"
+  version "1.9.3"
+  sha256 "668a42212b9dcb253f308ba6137deaa8616dece83135635b8c9ce7ff8339014c"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/04.iosevka-slab-#{version}.zip"
   name "Iosevka Slab"
@@ -8,6 +8,6 @@ cask "font-iosevka-slab" do
   license :ofl
 
   Dir["#{caskroom_path}/#{version}/*.ttf"].each do |f|
-    font f, target: "iosevka-slab/#{f.gsub("#{caskroom_path}/#{version}/", "")}"
+    font f, target => "iosevka-slab/#{f.gsub("#{caskroom_path}/#{version}/", "")}"
   end
 end
--- casks/font-iosevka.rb
+++ casks/font-iosevka.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka" do
-  version "1.9.1"
-  sha256 "aa75fc2365fe302ade7a4bf2eb575519e60b61dbccc94c46983f4fa62f1c2ad7"
+  version "1.9.3"
+  sha256 "9ea3796fe4c483b45cf52a23657a1adb3b0b6e3cb7b3ebc285435eb63cd84f04"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/01.iosevka-#{version}.zip"
   name "Iosevka"
@@ -8,6 +8,6 @@ cask "font-iosevka" do
   license :ofl
 
   Dir["#{caskroom_path}/#{version}/*.ttf"].each do |f|
-    font f, target: "iosevka/#{f.gsub("#{caskroom_path}/#{version}/", "")}"
+    font f, target => "iosevka/#{f.gsub("#{caskroom_path}/#{version}/", "")}"
   end
 end
